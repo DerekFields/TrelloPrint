f3769cbec87f75c9c36931038cb6beab40d2ba5b
Derek Fields <derek@fields.name>
2015-09-20 20:10:49 -0400

    Created initial CardTemplate layout

--- TrelloPrint/main.py
+++ TrelloPrint/main.py
@@ -6,7 +6,6 @@ API_KEY = 'cc0f13b21cedc64a5a64878eda654ce8'
 ### The AUTH key needs to be reset. Currently done manually
 AUTH_KEY = '8b26e0e0831556feead41d7fc3af5debc77a2984faad5543960272da6410b246'
 PKB_ID = '55dd03c28b4efd740fd64e69'
-# Secret:a3dbbc2923da49117c1ac34c14292fa8b40e1c78e8ea30bb55c500dc30d6782b
 
 def main():
     tapi = trello.TrelloApi(API_KEY)
